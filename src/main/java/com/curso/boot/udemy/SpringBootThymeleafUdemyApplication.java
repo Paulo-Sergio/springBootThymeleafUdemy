8deb4dab04d9a64f58a3b1403b517b3c71d70fe9
Paulo-Sergio <paulofranca.contato@gmail.com>
2019-03-23 23:04:29 -0300

    estrutura inicial da aplicação com paginas estáticas

---
+++ src/main/java/com/curso/boot/udemy/SpringBootThymeleafUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.curso.boot.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafUdemyApplication.class, args);
+	}
+
+}
