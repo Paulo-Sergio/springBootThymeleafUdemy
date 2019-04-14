ac2877652a26e57face230c569c4d65115e9ef0e
Paulo-Sergio <paulofranca.contato@gmail.com>
2019-04-14 15:35:16 -0300

    funcionalidade para cadastrar funcionario

---
+++ src/main/java/com/curso/boot/udemy/web/conversor/StringToCargoConverter.java
@@ -0,0 +1,25 @@
+package com.curso.boot.udemy.web.conversor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.curso.boot.udemy.domain.Cargo;
+import com.curso.boot.udemy.service.CargoService;
+
+@Component
+public class StringToCargoConverter implements Converter<String, Cargo> {
+
+	@Autowired
+	private CargoService service;
+	
+	@Override
+	public Cargo convert(String text) {
+		if (text.isEmpty()) {
+			return null;
+		}
+		Long id = Long.valueOf(text);
+		return service.buscarPorId(id);
+	}
+
+}
